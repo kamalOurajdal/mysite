ac45f3cd577de2df64bc48646769315b9ab9e33b
Kamal Ourajdal <kamalourajdal@berexia.com>
2025-07-05 21:15:02 +0100

    Enhance admin interface and model string representation
    - Added QuestionAdmin class to customize the admin interface for Question model with list display, filters, and search fields.
    - Implemented __str__ method for Question and Choice models for better string representation.

--- polls/admin.py
+++ polls/admin.py
@@ -4,6 +4,10 @@ from .models import Question, Choice
 
 # Register your models here.
 
+class QuestionAdmin(admin.ModelAdmin):
+    list_display  = ('id', 'question_text', 'pub_date')  # columns in the list view
+    list_filter   = ('pub_date',)                        # sidebar filters (optional)
+    search_fields = ('question_text',)
 
 admin.site.register(Question)
 admin.site.register(Choice)
