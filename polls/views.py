d57fe5357ca6c680629cd598c97e9b436ea29db5
Kamal Ourajdal <kamalourajdal@berexia.com>
2025-07-05 21:35:13 +0100

    Add polls app views and templates for question listing and details

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,32 @@
+from django.http.request import HttpRequest
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
+
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.all()
+    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
+
+
+def owner(request: HttpRequest) -> HttpResponse:
+    response = HttpResponse()
+    response.write("Hello, world. 1aa4396d is the polls index.")
+    return response
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
